70f6fdd785ceae5f4aa597a3042126d9d6d60412
FlavianMiano <48262134+FlavianMiano@users.noreply.github.com>
2023-04-11 16:13:14 +0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -80,64 +80,64 @@ from forex_python.converter import CurrencyRates
 
 
 st.subheader('Stocks Today :zap:')
-import requests
-resp = requests.get("https://economictimes.indiatimes.com/markets/stocks/rssfeeds/2146842.cms")
+# import requests
+# resp = requests.get("https://economictimes.indiatimes.com/markets/stocks/rssfeeds/2146842.cms")
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(resp.content, features='xml')
-#st.write(soup.findAll('title'))
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup(resp.content, features='xml')
+# #st.write(soup.findAll('title'))
 
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
 
-headlines =  soup.findAll('title')
+# headlines =  soup.findAll('title')
 
-processed_hline = nlp(headlines[4].text)
+# processed_hline = nlp(headlines[4].text)
 
-companies = []
+# companies = []
 
-for title in headlines:
-    doc = nlp(title.text)
-    for token in doc.ents:
-        if token.label_ == 'ORG':
-            companies.append(token.text)
-        else:
-            pass
+# for title in headlines:
+#     doc = nlp(title.text)
+#     for token in doc.ents:
+#         if token.label_ == 'ORG':
+#             companies.append(token.text)
+#         else:
+#             pass
 
-#st.write(companies)
+# #st.write(companies)
 
-## collect various market attributes of a stock
-stock_dict = {
-    'Org': [],
-    'Symbol': [],
-    'currentPrice': [],
-    'dayHigh': [],
-    'dayLow': [],
-    'forwardPE': [],
-    'dividendYield': []
-}
+# ## collect various market attributes of a stock
+# stock_dict = {
+#     'Org': [],
+#     'Symbol': [],
+#     'currentPrice': [],
+#     'dayHigh': [],
+#     'dayLow': [],
+#     'forwardPE': [],
+#     'dividendYield': []
+# }
 
-## for each company look it up and gather all market data on it
-stocks_df = pd.read_csv("ind_nifty500list.csv")
+# ## for each company look it up and gather all market data on it
+# stocks_df = pd.read_csv("ind_nifty500list.csv")
 
-for company in companies:
-    try:
-        if stocks_df['Company Name'].str.contains(company).sum():
-            symbol = stocks_df[stocks_df['Company Name'].\
-                                str.contains(company)]['Symbol'].values[0]
-            org_name = stocks_df[stocks_df['Company Name'].\
-                                str.contains(company)]['Company Name'].values[0]
-            stock_dict['Org'].append(org_name)
-            stock_dict['Symbol'].append(symbol)
-            stock_info = yf.Ticker(symbol+".NS").info
-            stock_dict['currentPrice'].append(stock_info['currentPrice'])
-            stock_dict['dayHigh'].append(stock_info['dayHigh'])
-            stock_dict['dayLow'].append(stock_info['dayLow'])
-            stock_dict['forwardPE'].append(stock_info['forwardPE'])
-            stock_dict['dividendYield'].append(stock_info['dividendYield'])
-        else:
-            pass
-    except:
-        pass
+# for company in companies:
+#     try:
+#         if stocks_df['Company Name'].str.contains(company).sum():
+#             symbol = stocks_df[stocks_df['Company Name'].\
+#                                 str.contains(company)]['Symbol'].values[0]
+#             org_name = stocks_df[stocks_df['Company Name'].\
+#                                 str.contains(company)]['Company Name'].values[0]
+#             stock_dict['Org'].append(org_name)
+#             stock_dict['Symbol'].append(symbol)
+#             stock_info = yf.Ticker(symbol+".NS").info
+#             stock_dict['currentPrice'].append(stock_info['currentPrice'])
+#             stock_dict['dayHigh'].append(stock_info['dayHigh'])
+#             stock_dict['dayLow'].append(stock_info['dayLow'])
+#             stock_dict['forwardPE'].append(stock_info['forwardPE'])
+#             stock_dict['dividendYield'].append(stock_info['dividendYield'])
+#         else:
+#             pass
+#     except:
+#         pass
 
 # ## create a dataframe to display the buzzing stocks
 # #pd.DataFrame(stock_dict)
